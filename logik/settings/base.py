48e8d3efaa134e1d5b054e2e3f27994381c95a21
David <davidlescanoar@gmail.com>
2020-06-21 23:46:42 -0300

    Update templates dir

--- logik/settings/base.py
+++ logik/settings/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'logik.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["/home/david/Documentos/Proyectos/logik/templates/"],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
