8b87f6f1da2d1eaff511a65c051ce42d7a0044c7
David <davidlescanoar@gmail.com>
2020-06-22 11:47:36 -0300

    Update domain

--- logik/settings_production.py
+++ logik/settings_production.py
@@ -1,7 +1,7 @@
 import dj_database_url
 from decouple import config
 
-ALLOWED_HOSTS = ['logik-web-app.herokuapp.com']
+ALLOWED_HOSTS = ['logik.com.ar']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
