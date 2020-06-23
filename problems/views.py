8db6d082e8a34efbb81494880a3db6721a6cbf04
DavidLescano <davidlescanoar@gmail.com>
2020-06-23 14:08:15 -0300

    Score color in problems

--- problems/views.py
+++ problems/views.py
@@ -3,12 +3,32 @@ from django.http import HttpResponse
 import json
 from problems.models import Problems
 
+def puntosObtenidos(solvedBy, username):
+    JSON=json.loads(solvedBy)
+
+    if username not in JSON:
+        return 0
+
+    return JSON[username]
+
+def asignarColor(puntaje):
+    if puntaje==0:
+        return "fila-roja"
+    if puntaje==100:
+        return "fila-verde"
+    return "fila-amarilla"
+
 def problems(request):
     problemas=Problems.objects.all()
 
     listaProblemas=[]
 
     for i in problemas:
-        listaProblemas.append((i.problem_name, i.problem_points, i.problem_link))
+        puntaje=puntosObtenidos(i.solvedBy, request.user.username)
+        color=asignarColor(puntaje)
+
+        listaProblemas.append((i.problem_name, i.problem_points, i.problem_link, puntaje, color))
+
+
 
     return render(request, 'problems.html', {'problemas':listaProblemas})
