2c1f5d2c46aae13852f6d7e7b227a53d010942a9
DavidLescano <davidlescanoar@gmail.com>
2020-06-23 01:47:00 -0300

    Correct debug variable

--- logik/settings.py
+++ logik/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='Secret Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
