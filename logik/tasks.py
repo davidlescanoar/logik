f6c0606583810c503f4c7a3b26aca75d7c4cc9a6
Gaston Fontenla <gastonfon@hotmail.com>
2020-07-21 13:12:30 -0300

    Refactorización tasks.py

--- logik/tasks.py
+++ logik/tasks.py
@@ -255,9 +255,6 @@ def update_ranking():
     #Usuarios
     users=User.objects.all()
 
-    vaciarSolvedBy()
-    return 1
-
     for user in users:
         cuenta=Account.objects.filter(Logik_Handle=user)
         if  cuenta.exists():
