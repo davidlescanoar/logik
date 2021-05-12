781d1e42f636e5c2e855b71fa02b86385f1e45b7
David Lescano <davidlescanoar@gmail.com>
2021-05-12 14:39:59 -0300

    Problem section

--- frontend/src/components/Problems/Problems.js
+++ frontend/src/components/Problems/Problems.js
@@ -1,14 +1,84 @@
 import React, {Component} from 'react';
 import Table from "../Table/Table";
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import API from "../../API";
+import Button from '@material-ui/core/Button';
 
-export default function Problems() {
-    return (
-        <div>
-            <Typography variant="h5" gutterBottom>
-                Problemas
-            </Typography>
-            <Table/>
-        </div>
-    );
+export default class Problems extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            table: {
+                rows: [],
+                columns: [],
+                order: 'desc',
+                orderBy: 'acceptance',
+            }
+        };
+    }
+
+    getProblemTable(problems) {
+        let problemList = [];
+        for (var i = 0; i < problems.length; i++) {
+            let name = problems[i].name;
+            let link = problems[i].link;
+            let user_points = problems[i].user_points;
+            let acceptance = problems[i].acceptance.toFixed(1) + "%";
+            var problem = [
+                [name, link],
+                [user_points, null],
+                [acceptance, null],
+            ];
+            problemList.push(problem);
+        }
+        return problemList;
+    }
+
+    getTable = async event => {
+        const response = await API.get("problems/?username=DavidLescano", {headers: {'Authorization': 'Token bf060d1b68da79687a887d26a93c7e12708e274b'}});
+        const problemList = this.getProblemTable(response.data);
+        this.setState({
+            table: {
+                ...this.state.table,
+                rows: problemList,
+                columns: [
+                    {
+                        id: 'name',
+                        numeric: false,
+                        disablePadding: true,
+                        label: 'Nombre'
+                    },
+                    {
+                        id: 'score',
+                        numeric: true,
+                        disablePadding: false,
+                        label: 'Puntaje'
+                    },
+                    {
+                        id: 'acceptance',
+                        numeric: true,
+                        disablePadding: false,
+                        label: 'Aceptación'
+                    },
+                ],
+            }
+        });
+
+    };
+
+    componentDidMount() {
+        this.getTable();
+    }
+
+    render() {
+        return (
+            <div>
+                <Typography variant="h5" gutterBottom>
+                    Problemas
+                </Typography>
+                <Table source={this.state.table}/>
+            </div>
+        )
+    }
 }
--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {useState, useEffect} from 'react';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
 import {Table as BasicTable} from '@material-ui/core';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,60 +7,279 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import PropTypes from 'prop-types';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
     },
-});
+    paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    },
+    table: {
+        minWidth: 750,
+        "& .MuiTableCell-sizeSmall": {
+            padding: "12px"
+        },
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+    },
+}));
 
-function createData(url, points, ac) {
-    return [url, points, ac];
-}
+const useStyles1 = makeStyles((theme) => ({
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+    },
+}));
 
-const cols = ['URL', 'Puntaje', 'AC'];
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Eclair', 262, 16.0),
-    createData('Cupcake', 305, 3.7),
-    createData('Gingerbread', 356, 16.0),
-];
-
-export default function Table() {
-    const classes = useStyles();
+function EnhancedTableHead(props) {
+    const {classes, order, orderBy, onRequestSort} = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
 
     return (
-        <TableContainer component={Paper}>
-            <BasicTable className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        {cols.map((col, index) => {
-                            return (
-                                <TableCell align={index ? "right" : "left"}>
-                                    {col}
-                                </TableCell>
-                            );
-                        })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => {
-                        return (
-                            <TableRow key={row}>
-                                {row.map((col, index) => {
-                                    return (
-                                        <TableCell component="th" scope="row" align={index ? "right" : "left"}>
-                                            {col}
-                                        </TableCell>
-                                    );
-                                })}
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </BasicTable>
-        </TableContainer>
+        <TableHead>
+            <TableRow>
+                {props.headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+function TablePaginationActions(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const {count, page, rowsPerPage, onChangePage} = props;
+
+    const handleFirstPageButtonClick = (event) => {
+        onChangePage(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+        onChangePage(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+        onChangePage(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <div className={classes.root}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
+            </IconButton>
+            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
+            </IconButton>
+        </div>
+    );
+}
+
+TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+};
+
+const preventDefault = (event) => event.preventDefault();
+
+function getSortFieldIndex(cols, orderBy){
+    for(var i=0;i<cols.length;i++){
+        if(cols[i].id==orderBy) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+function descendingComparator(a, b, orderBy, index, columns) {
+    let numeric = columns[index].numeric;
+    let A = a[index][0];
+    let B = b[index][0];
+    if(numeric){
+        A=parseFloat(A);
+        B=parseFloat(B);
+    }
+    if (B < A) {
+        return -1;
+    }
+    if (B > A) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy, index, columns) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy, index, columns)
+        : (a, b) => -descendingComparator(a, b, orderBy, index, columns);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+function getElement(item) {
+    let name = item[0];
+    let url = item[1];
+    if (url != null) {
+        return <Link href={url} target="_blank">{name}</Link>;
+    }
+    return name;
+}
+
+export default function Table(props) {
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [selected, setSelected] = React.useState([]);
+    const [order, setOrder] = React.useState(props.source.order);
+    const [orderBy, setOrderBy] = React.useState(props.source.orderBy);
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, props.source.rows.length - page * rowsPerPage);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <BasicTable className={classes.table} size="small" aria-label="simple table">
+                    <EnhancedTableHead
+                        classes={classes}
+                        numSelected={selected.length}
+                        order={order}
+                        orderBy={orderBy}
+                        onRequestSort={handleRequestSort}
+                        rowCount={props.source.rows.length}
+                        headCells={props.source.columns}
+                    />
+                    <TableBody>
+                        {stableSort(props.source.rows, getComparator(order, orderBy, getSortFieldIndex(props.source.columns,orderBy), props.source.columns))
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row, index) => {
+                                return (
+                                    <TableRow key={index}>
+                                        {row.map((col, index) => {
+                                            return (
+                                                <TableCell component="th" scope="row" align={index ? "right" : "left"}
+                                                           key={index}>
+                                                    {getElement(col)}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+                                colSpan={3}
+                                count={props.source.rows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                labelRowsPerPage="Filas por página"
+                                labelDisplayedRows={({from, to, count}) => `Mostrando ${from}-${to} de ${count}`}
+                                onChangePage={handleChangePage}
+                                onChangeRowsPerPage={handleChangeRowsPerPage}
+                                ActionsComponent={TablePaginationActions}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </BasicTable>
+            </TableContainer>
+        </div>
     );
 }
